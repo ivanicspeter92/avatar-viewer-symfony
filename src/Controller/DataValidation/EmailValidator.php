5b288872f3ee42436d7c23b920c27c51d7b9e8a8
Peter Ivanics <peter@choicely.com>
2018-09-28 20:13:57 +0200

    Simplification

--- src/Controller/DataValidation/EmailValidator.php
+++ src/Controller/DataValidation/EmailValidator.php
@@ -11,7 +11,7 @@ class EmailValidator extends Validator
 {
     public static function validate($value)
     {
-        if (is_string($value) && $value != "")
+        if (is_string($value))
         {
             $regex = "/[A-Z0-9a-z._%+-]{2,64}+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}/";
             $matching_result = preg_match($regex, $value);
