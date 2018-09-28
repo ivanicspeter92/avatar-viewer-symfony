7653c5355f363bcc517543334f96dc23faea2768
Peter Ivanics <peter@choicely.com>
2018-09-28 20:06:25 +0200

    Fixed return value evaluation

--- src/Controller/DataValidation/EmailValidator.php
+++ src/Controller/DataValidation/EmailValidator.php
@@ -16,7 +16,7 @@ class EmailValidator extends Validator
             $regex = "/[A-Z0-9a-z._%+-]{2,64}+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}/";
             $matching_result = preg_match($regex, $value);
 
-            return $matching_result == TRUE;
+            return $matching_result == 1;
         }
         return false;
     }
