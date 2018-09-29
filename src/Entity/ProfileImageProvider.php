9e12667edfabfd5c0c5230224689dbb3c13bcf95
Peter Ivanics <peter@choicely.com>
2018-09-29 13:21:07 +0200

    Added documentation

--- src/Entity/ProfileImageProvider.php
+++ src/Entity/ProfileImageProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Entity;
 
+/**
+ * Class ProfileImageProvider An enumerator class for a set of profile image providers.
+ * @package App\Entity
+ */
 abstract class ProfileImageProvider
 {
     const Gravatar = 0;
