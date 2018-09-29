e018df1128da8d7ad02cd2963777ff3c8d9d25e4
Peter Ivanics <peter@choicely.com>
2018-09-29 18:24:53 +0200

    Docstrings

--- src/Controller/IO/JSONDataLoader.php
+++ src/Controller/IO/JSONDataLoader.php
@@ -3,10 +3,17 @@
 namespace App\Controller\IO;
 
 use Doctrine\Instantiator\Exception\UnexpectedValueException;
+use PHPUnit\Runner\Exception;
 use Symfony\Component\Dotenv\Exception\PathException;
 
 class JSONDataLoader implements RawDataLoader
 {
+    /**
+     * @param $path string The absolute or relative path to read the data from.
+     * @return mixed; an associative array (dictionary) or an array of associative arays (dictionaries)
+     * @throws PathException if the path does not exist or is not readable.
+     * @throws UnexpectedValueException if the desired file is not in JSON format.
+     */
     public static function loadJSONFileContentsAtPath($path)
     {
         $contents = @file_get_contents($path);
